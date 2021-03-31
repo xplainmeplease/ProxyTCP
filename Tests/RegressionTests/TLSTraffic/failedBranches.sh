d9513e7e19f02d81d36c7f3f5701b8a7b30a7a68
Edgar Cloud <fluent0x@gmail.com>
2021-03-31 11:03:44 -0400

    Add sudo to regtests

--- Tests/RegressionTests/TLSTraffic/failedBranches.sh
+++ Tests/RegressionTests/TLSTraffic/failedBranches.sh
@@ -1,3 +1,3 @@
-./../../bin/proxytcp --mode replay --output /root/tmp.pcap --port 1080
-./../../bin/proxytcp --mode replay
-./../../bin/proxytcp --mode invalidmode
+sudo ./../../bin/proxytcp --mode replay --output /root/tmp.pcap --port 1080
+sudo ./../../bin/proxytcp --mode replay
+sudo ./../../bin/proxytcp --mode invalidmode
